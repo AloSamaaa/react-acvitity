3159da9c56f414abd14669b695ebabe0f176eeb5
AloSamaa <kaizkiegt@gmail.com>
2022-12-15 22:22:03 +0800

    not found page alignment

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return (
         <>
-            <h1>404 page not found</h1>
+            <h1 className="text-center mt-5">404 page not found</h1>
         </>
     )
 }
