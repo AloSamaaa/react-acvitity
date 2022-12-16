0012973912243719c478edcb1eeb64d1e03ddf22
AloSamaa <kaizkiegt@gmail.com>
2022-12-16 09:31:47 +0800

    arranged code

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,11 +1,11 @@
 export default function NotFound() {
     return (
         <>
-            <div className="container">
+            <footer className="container">
                 <div className="fixed-bottom pb-5 text-center d-none d-xl-block">
                 Developed by <strong>Group 6</strong>
                 </div>
-            </div>
+            </footer>
         </>
     )
 }
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-const Post = ({ post }) => (
-  <div className="col-11 border mx-auto shadow-lg ps-5 pt-5 pb-5 mb-5 mt-3 pe-5">
-    <h3>{post.title}</h3>
-    <br/>
-    <p>{post.body}</p>
-  </div>
-);
-
 const Posts = () => {
   const [posts, setPosts] = useState([]);
 
@@ -16,6 +8,14 @@ const Posts = () => {
       .then(response => response.json())
       .then(data => setPosts(data));
   }, []);
+
+  const Post = ({ post }) => (
+    <div className="col-11 border mx-auto shadow-lg ps-5 pt-5 pb-5 mb-5 mt-3 pe-5">
+      <h3>{post.title}</h3>
+      <br />
+      <p>{post.body}</p>
+    </div>
+  );
 
   return (
     <div>
